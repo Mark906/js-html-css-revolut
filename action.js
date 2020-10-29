2e3bc2137991537456b21e7d5297dfeaea4564ac
Marco <markoka1@outlook.it>
2020-10-29 16:56:21 +0100

    correzione bug

--- action.js
+++ action.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $(".into-navbar-right ul li").mouseenter(function(){
     $(".dropdown-menu").css("display", "block");
   });
-  $(".into-navbar-right ul li").mouseleave(function(){
+  $("#dropdown").mouseleave(function(){
     $(".dropdown-menu").css("display", "none");
   });
 });
